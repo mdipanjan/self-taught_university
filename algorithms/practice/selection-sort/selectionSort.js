d5ad86a3e96309631731893bc86f98286304c577
mdipanjan <mdipanjan33@gmail.com>
2019-10-17 03:40:40 -1200

    sorting-practtice

---
+++ algorithms/practice/selection-sort/selectionSort.js
@@ -0,0 +1,72 @@
+// arrBckup = [112,45,89,78,65,35,85,69,25,250,47,38,96,125,254,190,110,50,300,5,14,29,61,33,98,29];
+arr = [312,45,89,78,65,35,85,69,25,250,47,38,96,125,254,190,110,50,300,5,14,29,61,33,98,29];
+let box = document.querySelector('.box');
+let interval;
+
+
+function selectionSort(a){
+    for(let i=0; i < a.length; i++){
+        setTimeout( function timer(){
+            let min = i;
+        
+            for(let l= i+1; l < a.length; l++){
+                if(arr[l] < arr[min]){
+                    min = l;
+                }
+            }
+            if(min !== i){
+                let temp = a[i];
+                a[i] = a[min];
+                a[min] = temp;
+            }
+            // console.log(arr)
+            drawLines(arr);
+        }, i*1000 );
+
+    }
+    
+}
+
+
+let sortStart = document.querySelector('.sort-start');
+    sortStart.addEventListener('click',function(){
+        selectionSort(arr);
+        drawLines(arr);
+
+    });
+
+let sortReset = document.querySelector('.sort-reset');
+    sortReset.addEventListener('click',function(){
+        // box.innerHTML = ''
+        window.location.reload()
+    });
+
+
+
+function drawLines(elem){
+   
+    let box = document.querySelector('.box');
+    let data = '';
+    for(var i=0; i < elem.length; i++){
+        data+= `<div class="bars"></div>`
+    }
+    box.innerHTML = data;
+    let bars = document.querySelectorAll('.bars');
+    bars.forEach(function(i,v){
+        i.style.height = elem[v]+'px';
+        //i.classList.add('b')
+    });
+    
+}
+document.onload =  drawLines(arr);
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    
+    slider.setAttribute('value', this.value);
+    interval = slider.getAttribute('value');
+    output.innerHTML = this.value;
+}
